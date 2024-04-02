53b360a4fc27519d9be580594ced61ac1a81371d
Flictivity <bulat_sa1@mail.ru>
2024-04-02 17:50:59 +0300

    Миграции для предыдущего коммита

---
+++ SoVet.Data/Migrations/20240402133942_AddIsDeletedFieldToFacilityCategory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoVet.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsDeletedFieldToFacilityCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_deleted",
+                table: "clients");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_deleted",
+                table: "facility_categories",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_deleted",
+                table: "facility_categories");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_deleted",
+                table: "clients",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
