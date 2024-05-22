7eb5800b6398107ed84f790bc99f76c773160640
Flictivity <bulat_sa1@mail.ru>
2024-05-22 22:20:52 +0300

    Исправлены недочеты

--- SoVet.BlazorWebClient/Models/BaseReference.cs
+++ SoVet.BlazorWebClient/Models/BaseReference.cs
@@ -1,7 +1,10 @@
-﻿namespace SoVet.BlazorWebClient.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoVet.BlazorWebClient.Models;
 
 public abstract class BaseReference
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Обязательное поле")]
     public string Name { get; set; } = null!;
 }
